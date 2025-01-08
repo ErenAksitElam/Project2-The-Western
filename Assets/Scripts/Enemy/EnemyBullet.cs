c73107eec1cd4e85a6e7e763ad1e5f46da26bc7f
ErenAksitElam <EAksit2024@elam.co.uk>
2025-01-08 12:44:36 +0000

    AHHH

--- Assets/Scripts/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Enemy/EnemyBullet.cs
@@ -6,6 +6,7 @@ public class EnemyBullet : MonoBehaviour
 {
     private Vector2 moveDirection;
     private float moveSpeed;
+    private Rigidbody2D rb;
 
     private void OnEnable()
     {
@@ -15,6 +16,7 @@ public class EnemyBullet : MonoBehaviour
     void Start()
     {
         moveSpeed = 5f;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -44,5 +46,11 @@ public class EnemyBullet : MonoBehaviour
     private void OnDisable()
     {
         CancelInvoke();
+    }
+
+    private void CheckDirection()
+    {
+        if (GetComponent<Rigidbody>().velocity.x > 0.3 || GetComponent<Rigidbody>().velocity.y > 0.3 || GetComponent<Rigidbody>().velocity.z > 0.3)
+            transform.LookAt(transform.position + GetComponent<Rigidbody>().velocity);
     }
 }
